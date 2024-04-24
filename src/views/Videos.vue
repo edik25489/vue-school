c8b8c6d8efc01db008d4fb94df28ecb4d1b348e3
Eduard_yakovlev <edik25mas@yandex.ru>
2024-04-24 13:34:39 +0300

    24.04

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,27 +1,42 @@
 <script setup lang="ts">
 import {useVideosStore} from "../store/videos.ts";
 import {useCategoriesStore} from "../store/categories.ts";
+import {computed, ref} from "vue";
 
 const videos = useVideosStore()
 const categories = useCategoriesStore()
+const setCategories = ref([]);
+const setVideos = computed({
+  get(){
+    return setCategories
+  },
+  set(){
+  }
+})
+
 </script>
 
 <template>
+  <div class="sm:container mt-12 w-screen mx-auto flex flex-row">
 
-  <div class="sm:container mt-12 max-w-md mx-auto flex flex-row justify-between">
-    <div class="flex basis-1/5">
+    <div class="container flex basis-1/5">
       <ul>
-        <li v-for="(ctg, ind) in categories.getCategories" :key="ind">
-          <p>{{ctg.name}}</p>
-        </li>
+        <div v-for="(ctg, ind) in categories.getCategories" :key="ind"
+              class="flex flex-row items-center justify-start gap-4 mt-4 p-2 border-b-2 border-r-2 hover:shadow-xl hover:cursor-pointer">
+          <input type="checkbox" :value=ctg.id v-model="setCategories">
+          <li>
+            <p>{{ctg.name}}</p>
+          </li>
+        </div>
       </ul>
     </div>
-    <div class="flex basis-4/5">
-      <ul class="flex flex-wrap">
+
+    <div class="container mx-auto flex basis-3/5">
+      <ul>
         <li v-for="(video, index) in videos.getVideos" :key="index"
-            class="flex flex-row max-w-md m-4 shadow-sm border rounded-md duration-300 hover:shadow-lg">
+            class="container flex flex-row sm:w-max-[500px] md:w-max-[1000px] p-4 m-4 shadow-sm border rounded-md duration-300 hover:shadow-lg">
           <a :href=video.src>
-            <img :src=video.img alt="описание" class="flex-none h-32 w-60 rounded-t-md object-cover"/>
+            <img :src=video.img alt="описание" class="flex-none h-32 w-60 rounded-md object-cover"/>
           </a>
           <div class="flex flex-col pt-3 pl-4 pr-2 pb-3">
             <h3 class="text-xl text-gray-900">
